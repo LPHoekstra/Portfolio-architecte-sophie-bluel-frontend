ae0bc9454c866a867eeca11737c67fea9b05ac80
MalBourbie <MalBourbie@gmail.com>
2024-09-10 14:00:04 +0200

    removed the gallery div on html. function for the filter menu that create the correct amount of button. createFigureElement is now filterWorks so the function creates the html for the works and does the filter. create a function for the GalleryConteneur. The works presentation page is now functional with all the expected features.

--- script/script.js
+++ script/script.js
@@ -1,25 +1,73 @@
 import { getWorks } from "./callApi.js";
 
-// create works element on html
-async function createFigureElement() {
+async function init() {
     const dataWorks = await getWorks()
+    const portfolio = document.getElementById("portfolio")
+    
+    function filterWorks(category) {
+        const gallery = document.querySelector(".gallery")
+        gallery.innerHTML = ""
+        
+        const filteredWorks = category === "Tous" ? dataWorks : dataWorks.filter(work => work.category.name === category)
+        
+        filteredWorks.forEach(work => {
+            const figure = document.createElement("figure")
+            const img = document.createElement("img")
+            const figcaption = document.createElement("figcaption")
+            
+            img.src = work.imageUrl
+            img.alt = work.title
+            figcaption.innerText = work.title
+            
+            figure.appendChild(img)
+            figure.appendChild(figcaption)
+            
+            gallery.appendChild(figure)
+        })
+    }
+    
+    function createCategoriesMenu() {
+        const conteneur = document.createElement("div")
+        
+        // add the btn "Tous"
+        const allBtn = document.createElement("button")
+        allBtn.innerText = "Tous"
+        allBtn.addEventListener("click", () => {
+            filterWorks(allBtn.innerText)
+        })
+        conteneur.appendChild(allBtn)
+        
+        const categoriesNameSet = new Set()
+        
+        // add all the uniques categories to the set
+        dataWorks.forEach(work => {
+            categoriesNameSet.add(work.category.name)
+        })
+        
+        // create a btn pour each categories
+        categoriesNameSet.forEach(category => {
+            const btn = document.createElement("button")
+            
+            btn.innerText = category
+            
+            btn.addEventListener("click", () => filterWorks(category))
+            
+            conteneur.appendChild(btn)
+        })
+        
+        portfolio.appendChild(conteneur)
+    }
+    
+    function createGalleryConteneur() {
+        const gallery = document.createElement("div")
+        gallery.classList.add("gallery")
+        portfolio.appendChild(gallery)
+    }
 
-    const gallery = document.querySelector("#portfolio .gallery")
-
-    dataWorks.forEach(work => {
-        const figure = document.createElement("figure")
-        const img = document.createElement("img")
-        const figcaption = document.createElement("figcaption")
-    
-        img.src = work["imageUrl"]
-        img.alt = work["title"]
-        figcaption.innerText = work["title"]
-    
-        figure.appendChild(img)
-        figure.appendChild(figcaption)
-    
-        gallery.appendChild(figure)
-    })
+    // create first the menu for filter, then the gallery conteneur, and show all the works by default
+    createCategoriesMenu()
+    createGalleryConteneur()
+    filterWorks("Tous")
 }
 
-createFigureElement()
+init()
