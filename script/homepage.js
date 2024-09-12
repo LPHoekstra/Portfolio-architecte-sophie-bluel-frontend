89102393606f2272d46f81a6d2a5d470f63bdc62
MalBourbie <MalBourbie@gmail.com>
2024-09-12 18:39:05 +0200

    implementation of the modal. When the user is logged and clicked on modify, the modal is displayed and he can close it by clicking on the close btn or when he clicks outside the modal.

--- script/homepage.js
+++ script/homepage.js
@@ -1,10 +1,12 @@
-import { worksPresentationHome } from "./worksPresentationHome.js";
+import { createCategoriesMenu, createGalleryConteneur, filterWorks } from "./worksPresentationHome.js";
 import { logoutBtn } from "./logout.js";
 import { isConnected } from "./isConnected.js";
+import { getWorks } from "./callApi.js";
+import { createModal, openModal } from "./modal.js";
 
 const connected = isConnected()
 
-worksPresentationHome()
+const dataWorks = await getWorks()
 
 // uptade home html for edition mode
 const editionMode = () => {
@@ -15,50 +17,57 @@ const editionMode = () => {
 const editModeHeader = () => {
     const mainConteneur = document.createElement("div")
     mainConteneur.classList.add("edition-conteneur")
-    
+
     const edit = document.createElement("span")
     edit.innerText = "Mode édition"
-    
+
     const editImg = document.createElement("img")
     editImg.src = "./assets/icons/edit.svg"
     editImg.alt = "Icon d'édition"
-    
+
     mainConteneur.appendChild(editImg)
     mainConteneur.appendChild(edit)
-    
+
     const body = document.querySelector("body")
-    
+
     body.prepend(mainConteneur)
 }
 
 const modifyBtn = () => {
     const editConteneur = document.createElement("div")
     editConteneur.classList.add("modify-conteneur__clickDiv")
-    
+
     const edit = document.createElement("span")
     edit.innerText = "Modifier"
-    
+
     const editImg = document.createElement("img")
     editImg.src = "./assets/icons/edit-black.svg"
     editImg.alt = "Icon d'édition"
-    
+
     editConteneur.appendChild(editImg)
     editConteneur.appendChild(edit)
-    
+
     const title = document.querySelector("#portfolio h2")
-    
+
     const mainConteneur = document.createElement("div")
     mainConteneur.classList.add("modify-conteneur")
-    
+
     mainConteneur.appendChild(title)
     mainConteneur.appendChild(editConteneur)
-    
+
     const portfolio = document.getElementById("portfolio")
-    
+
     portfolio.prepend(mainConteneur)
 }
 
 if (connected) {
     logoutBtn()
     editionMode()
+    createModal(dataWorks)
+
+    document.querySelector(".modify-conteneur__clickDiv").addEventListener("click", () => openModal())
+} else {
+    createCategoriesMenu(dataWorks)
 }
+createGalleryConteneur()
+filterWorks("Tous", dataWorks)
---
+++ script/modal.js
@@ -0,0 +1,65 @@
+export const createModal = (dataWorks) => {
+    const aside = document.createElement("aside")
+    aside.setAttribute("id", "modal-hidden")
+    aside.classList.add("modal")
+    aside.addEventListener("click", (event) => {
+        aside.setAttribute("id", "modal-hidden")
+    })
+
+    const close = document.createElement("img")
+    close.src = "./assets/icons/close.svg"
+    close.alt = "Fermer la fenêtre"
+    close.classList.add("modal-wrapper__close")
+    close.addEventListener("click", () => {
+        aside.setAttribute("id", "modal-hidden")
+    })
+
+    const title = document.createElement("h2")
+    title.innerText = "Galerie photo"
+
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("modal-wrapper")
+    wrapper.addEventListener("click", (event) => {
+        event.stopPropagation()
+    })
+
+    const imgWrapper = document.createElement("div")
+    imgWrapper.classList.add("imgWrapper")
+
+    const btn = document.createElement("button")
+    btn.classList.add("btn")
+    btn.classList.add("form__btn") // temporary
+    btn.innerText = "Ajouter une photo"
+
+    dataWorks.forEach(work => {
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
+        const trash = document.createElement("img")
+
+        trash.src = "./assets/icons/trash.svg"
+        trash.alt = "Supprimer la photo"
+        trash.classList.add("modal-wrapper__trash")
+
+        img.src = work.imageUrl
+        img.alt = work.title
+        img.classList.add("modal-wrapper__picture")
+
+        figure.appendChild(trash)
+        figure.appendChild(img)
+
+        imgWrapper.appendChild(figure)
+    })
+
+    wrapper.appendChild(close)
+    wrapper.appendChild(title)
+    wrapper.appendChild(imgWrapper)
+    wrapper.appendChild(btn)
+
+    aside.appendChild(wrapper)
+    document.querySelector("body").appendChild(aside)
+}
+
+export const openModal = () => {
+    const modal = document.querySelector(".modal")
+    modal.removeAttribute("id")
+}
